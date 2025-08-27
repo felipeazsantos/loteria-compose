88df56963e52aaadeef74c3fba9cc8532d501412
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-27 13:56:24 -0300

    creating dropdown component

--- app/src/main/java/dev/felipeazsantos/loteriacompose/MainActivity.kt
+++ app/src/main/java/dev/felipeazsantos/loteriacompose/MainActivity.kt
@@ -1,14 +1,24 @@
 package dev.felipeazsantos.loteriacompose
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.felipeazsantos.loteriacompose.compose.LoteriaApp
 import dev.felipeazsantos.loteriacompose.data.AppDatabase
 import dev.felipeazsantos.loteriacompose.data.Bet
+import dev.felipeazsantos.loteriacompose.ui.component.AutoTextDropDown
 import dev.felipeazsantos.loteriacompose.ui.theme.LoteriaComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +27,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoteriaComposeTheme {
-                LoteriaApp()
+//                LoteriaApp()
+
+                val list = listOf("Magesena", "quina", "lotofacil")
+
+                Column(modifier = Modifier.fillMaxSize().padding(20.dp)) {
+//                    AutoTextDropDown(list = list)
+                }
             }
         }
     }
--- app/src/main/java/dev/felipeazsantos/loteriacompose/ui/component/Component.kt
+++ app/src/main/java/dev/felipeazsantos/loteriacompose/ui/component/Component.kt
@@ -7,13 +7,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,9 +32,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import dev.felipeazsantos.loteriacompose.R
 import dev.felipeazsantos.loteriacompose.ui.theme.Green
 
@@ -93,6 +103,52 @@ fun LoItemType(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AutoTextDropDown(
+    label: String,
+    initial: String,
+    list : List<String>,
+    onItemChanged: (String) -> Unit
+) {
+    var dropDownExpanded by remember { mutableStateOf(false) }
+    var textFieldValue by remember { mutableStateOf(TextFieldValue(initial)) }
+
+    ExposedDropdownMenuBox(expanded = dropDownExpanded, onExpandedChange = {
+        dropDownExpanded = !dropDownExpanded
+    }) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth().menuAnchor(type = MenuAnchorType.PrimaryNotEditable),
+            value = textFieldValue,
+            readOnly = true,
+            label = {
+                Text(text = label)
+            },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = dropDownExpanded)
+            },
+            onValueChange = {
+                textFieldValue = it
+            }
+        )
+
+        ExposedDropdownMenu(
+            expanded = dropDownExpanded,
+            onDismissRequest = {
+                dropDownExpanded = false
+            }
+        ) {
+            list.forEach { item ->
+                DropdownMenuItem(text = { Text(item) }, onClick = {
+                    textFieldValue = TextFieldValue(item)
+                    onItemChanged(item)
+                    dropDownExpanded = false
+                })
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun LoNumberTextFieldPreview() {
@@ -114,4 +170,16 @@ fun LoItemTypePreview() {
         color = Color.White,
         bgColor = Green
     )
+}
+
+@Preview
+@Composable
+fun AutoTextDropDownPreview() {
+    AutoTextDropDown(
+        label = "",
+        initial = "",
+        list = listOf("test", "test2")
+    ) {
+
+    }
 }
